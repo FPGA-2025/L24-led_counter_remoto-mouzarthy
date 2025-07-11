56ff78bf976c3217bee3a6bc1abf73df6ccb2b6e
mouzarthy <mouzarthy2007@hotmail.com>
2025-07-11 17:06:27 -0300

    24-led counter

--- led_counter.v
+++ led_counter.v
@@ -6,5 +6,30 @@ module Counter #(
     output wire [7:0] leds
 );
 
+localparam ONE_SECOND  = CLK_FREQ;
+localparam HALF_SECOND = CLK_FREQ / 2;
+
+reg [31:0] counter;
+
+always @( posedge clk ) 
+begin
+    if(!rst_n) 
+    begin
+        counter <= 32'h0;
+        leds    <= 8'b0;
+    end 
+    else 
+    begin
+        if( counter >= HALF_SECOND - 1 ) 
+        begin
+            counter <= 32'h0;
+            leds    <= leds + 1;
+        end 
+        else 
+        begin
+            counter <= counter + 1;
+        end
+    end
+end
 
 endmodule
